f57183c29f68de4292619f15cbca4cf7235a75f0
Mauro Cicerchia <maurocicerchia98@gmail.com>
2025-08-26 20:53:26 -0300

    feat: add account mnemonic

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,9 @@ module.exports = {
   solidity: '0.7.3',
   networks: {
     hardhat: {
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
       chainId: 1337,
     },
   },
