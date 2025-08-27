1459eb77002bdfc1bb08a1b1a75aae7c4f60fcc1
Mauro Cicerchia <maurocicerchia98@gmail.com>
2025-08-27 00:43:02 -0300

    feat: sepolia deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require('@nomiclabs/hardhat-waffle')
 require('./tasks/faucet')
+require('dotenv').config()
+
+const { SEPOLIA_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -7,6 +10,11 @@ require('./tasks/faucet')
 module.exports = {
   solidity: '0.7.3',
   networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      chainId: 11155111, // Sepolia
+    },
     hardhat: {
       accounts: {
         mnemonic: "test test test test test test test test test test test junk",
